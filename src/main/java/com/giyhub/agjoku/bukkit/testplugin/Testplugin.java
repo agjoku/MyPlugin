19511bffeffc775b9bb317b69c31280c78c7c2fc
agjoku <kota.319.meado@gmail.com>
2022-03-10 15:59:15 +0900

    動作確認用

--- src/main/java/com/giyhub/agjoku/bukkit/testplugin/Testplugin.java
+++ src/main/java/com/giyhub/agjoku/bukkit/testplugin/Testplugin.java
@@ -1,12 +1,22 @@
 package com.giyhub.agjoku.bukkit.testplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockCookEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Testplugin extends JavaPlugin {
+public final class Testplugin extends JavaPlugin implements Listener {
+
+    @EventHandler
+    public void onBlockCook(BlockCookEvent e) {
+        Bukkit.broadcastMessage("料理ができました");
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this,this);
+        System.out.println("テストプラグインが有効になりました");
 
     }
 

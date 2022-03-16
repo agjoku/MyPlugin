f52f330d15f6f56cf6d3bcef6637d8b4d18f6db8
agjoku <kota.319.meado@gmail.com>
2022-03-17 00:21:45 +0900

    InventoryClickEventの拡張

--- src/main/java/com/giyhub/agjoku/bukkit/testplugin/Testplugin.java
+++ src/main/java/com/giyhub/agjoku/bukkit/testplugin/Testplugin.java
@@ -2,22 +2,21 @@ package com.giyhub.agjoku.bukkit.testplugin;
 
 import org.bukkit.*;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockCookEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-import static org.bukkit.Material.IRON_INGOT;
+import static org.bukkit.Material.*;
 
 public final class Testplugin<meta> extends JavaPlugin implements Listener {
 
@@ -50,7 +49,11 @@ public final class Testplugin<meta> extends JavaPlugin implements Listener {
         if(event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
             if(player.getInventory().getItemInMainHand().getType() == Material.DIAMOND) {
                 player.openInventory(inv);
-                inv.setItem(1,item);
+                inv.setItem(0,iron_ingot);
+                inv.setItem(2,gold_ingot);
+                inv.setItem(4,emerald);
+                inv.setItem(6,diamond);
+                inv.setItem(8, nephrite_ingot);
             }
         }
     }
@@ -59,13 +62,41 @@ public final class Testplugin<meta> extends JavaPlugin implements Listener {
     public void InventoryClickEvent(InventoryClickEvent event) {
         Player player = (Player)event.getWhoClicked();
         if(event.getCurrentItem().getType().equals(IRON_INGOT)){
-            event.getWhoClicked().sendMessage("バーカ");
+            player.sendMessage("バーカ");
+            player.closeInventory();
         }
+        if(event.getCurrentItem().getType().equals(GOLD_INGOT)) {
+            player.giveExpLevels(100);
+            player.closeInventory();
+            player.sendMessage("レベルを100LV与えました");
+
+        }
+        if(event.getCurrentItem().getType().equals(EMERALD)) {
+            player.addPotionEffect(new PotionEffect(potionEffectType,2000,100));
+            player.closeInventory();
+            player.sendMessage("幸運レベル100を100秒付与しました");
+        }
+        if(event.getCurrentItem().getType().equals(DIAMOND)) {
+            player.setGameMode(GameMode.CREATIVE);
+            player.closeInventory();
+            player.sendMessage("ゲームモードをクリエイティブに変更しました");
+        }
+        if(event.getCurrentItem().getType().equals(NETHERITE_INGOT)) {
+            player.kickPlayer("サーバーから退出しました。\nまた来てね(´・ω・｀)");
+            Bukkit.broadcastMessage(player.getName() + "が退出しました");
+        }
+
 
     }
 
     Inventory inv = Bukkit.createInventory(null, 9, "⊂二二二（  ＾ω＾）二⊃ﾌﾞｰﾝ");
-    ItemStack item = new ItemStack(IRON_INGOT);
+    ItemStack iron_ingot = new ItemStack(IRON_INGOT);
+    ItemStack gold_ingot = new ItemStack(GOLD_INGOT);
+    ItemStack emerald = new ItemStack(EMERALD);
+    ItemStack diamond = new ItemStack(DIAMOND);
+    ItemStack nephrite_ingot = new ItemStack(NETHERITE_INGOT);
+
+    PotionEffectType potionEffectType = PotionEffectType.LUCK;
 
 
     @Override
